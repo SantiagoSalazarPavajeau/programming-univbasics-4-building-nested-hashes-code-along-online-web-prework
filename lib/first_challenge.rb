b457213eb20e83262751881dec6a872b85bac8c8
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-09-14 20:07:06 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,6 +5,10 @@ def first_challenge
        :hero => {},
        :hero_friends => []
      },
-     :capulet => {}
+     :capulet => {:patriarch => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => []
+     }
    }
 end
