c0ec125cc844dbf261ea307122d0d8ef4204e55e
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-09-14 20:45:13 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -3,12 +3,8 @@ def first_challenge
      :montague => {:patriarch => {:name => "Lord Montague", :age => "53"},
        :matriarch => {:name => "Lady Montage", :age => "54"},
        :hero => {:name =>"Romeo", :age => "15", :status => "alive"},
-       :hero_friends => [{:name => "Benvolio",
-       :age => "17",
-       :attitude => "worried"},
-       {:name => "Mercutio",
-       :age => "18",
-       :attitude => "hot-headed"
+       :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"},
+       {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
        ]
      },
      :capulet => {:patriarch => {:name => "Lord Capulet", :age => "50"},
