d333f9839c242d446db963dff3ea3234116fc9b9
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-09-14 21:05:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,5 @@
 def first_challenge
-   epic_tragedy = { :montague => 
+   epic_tragedy = { :montague => {
        :patriarch => {:name => "Lord Montague", :age => "53"},
        :matriarch => {:name => "Lady Montage", :age => "54"},
        :hero => {:name =>"Romeo", :age => "15", :status => "alive"},
@@ -12,5 +12,6 @@ def first_challenge
        :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
        :heroine_friends => [{:name => "Steven", :age => "30", :attitude =>"confused"},{:name => "Nurse", :age => "44", :attitude => "worried"}
        ]
+     }
      }
 end
